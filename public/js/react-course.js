54a21f6f01be647b872fcdabd4d07e9582f7e3dc
SeryiBaran <seryibaranminepy@yandex.ru>
2022-01-19 16:33:37 +0300

    Правки из Pull Request

--- public/js/react-course.js
+++ public/js/react-course.js
@@ -1,12 +1,12 @@
 const linksForCopy = document.querySelectorAll('.link_for_copy');
-const linksForCopyFunc = 'CopyToClipboard(this.previousElementSibling.href)';
-const emailsForCopyFunc =
-  "CopyToClipboard(this.previousElementSibling.href.replace('mailto:', ''))";
 
-let isLinkEmail = false;
+// TODO: try to simplify in future
+const linksForCopyFunc = 'copyToClipboard(this.previousElementSibling.href)';
+const emailsForCopyFunc =
+  "copyToClipboard(this.previousElementSibling.href.replace('mailto:', ''))";
 
 for (let i = 0; i < linksForCopy.length; i++) {
-  isLinkEmail = linksForCopy[i].getAttribute('href').indexOf('mailto:') != -1;
+  const isLinkEmail = linksForCopy[i].getAttribute('href').indexOf('mailto:') != -1;
   linksForCopy[i].insertAdjacentHTML(
     'afterEnd',
     '<button class="copyButton" onclick="' +
@@ -15,11 +15,11 @@ for (let i = 0; i < linksForCopy.length; i++) {
   );
 }
 
-function CopyToClipboard(arg) {
-  var aux = document.createElement('input');
-  aux.setAttribute('value', arg);
-  document.body.appendChild(aux);
-  aux.select();
+function copyToClipboard(textForCopy) {
+  let tempInput = document.createElement('input');
+  tempInput.setAttribute('value', textForCopy);
+  document.body.appendChild(tempInput);
+  tempInput.select();
   document.execCommand('copy');
-  document.body.removeChild(aux);
+  document.body.removeChild(tempInput);
 }
