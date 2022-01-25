3e3aedb3f66e22720597333aa11f413afa27eba8
SeryiBaran <seryibaranminepy@yandex.ru>
2022-01-25 21:08:05 +0300

    Add Gulp (webpack not working)

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,42 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
+import groupCssMediaQueries from 'gulp-group-css-media-queries';
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+  return app.gulp.src(app.path.src.scss, { soursemaps: app.isDev })
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: 'SCSS',
+        message: 'Error: <%= error.message %>'
+      })
+    ))
+    .pipe(sass({
+      outoutStyle: 'expanded'
+    }))
+    .pipe(app.plugins.if(
+      app.isBuild,
+      groupCssMediaQueries()
+    ))
+    .pipe(autoprefixer({
+      grid: true,
+      overrideBrowserslist: ['last 3 versions'],
+      cascade: true
+    }))
+    // Раскомментировать, если нужен не сжатый дубликат файла стилей
+    // .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.if(
+      app.isBuild,
+      cleanCss()
+    ))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream());
+};
