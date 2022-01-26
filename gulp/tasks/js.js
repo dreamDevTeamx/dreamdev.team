58bf0bdd3ba179a08ea9b3beff029d74af6260ec
SeryiBaran <seryibaranminepy@yandex.ru>
2022-01-26 15:01:23 +0300

    Delete Webpack

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,19 +1,4 @@
-import webpack from 'webpack-stream';
-
 export const js = () => {
-  return app.gulp.src(app.path.src.js, { soursemaps: app.isDev })
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: 'JS',
-        message: 'Error: <%= error.message %>'
-      })
-    ))
-    .pipe(webpack({
-      mode: app.isBuild ? 'production' : 'development',
-      output: {
-        filename: 'main.min.js'
-      }
-    }))
-    .pipe(app.gulp.dest(app.path.build.js))
-    .pipe(app.plugins.browsersync.stream());
+  return app.gulp.src(app.path.src.js)
+    .pipe(app.gulp.dest(app.path.build.js));
 };
